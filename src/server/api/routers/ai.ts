19a01b053779c27a64ce1f458d8461f14684fd0f
Szarvas Péter <peterszarvas94@gmail.com>
2023-04-25 22:34:08 +0200

    add: basic structure

---
+++ src/server/api/routers/ai.ts
@@ -0,0 +1,68 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { openai } from "~/utils/openai";
+
+export const aiRouter = createTRPCRouter({
+  getChatCompletion: publicProcedure
+    .input(z.object({ prompt: z.string() }))
+    .mutation(async ({ input }) => {
+      console.log("input", input);
+
+      const chatCompletion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo-0301",
+        messages: [
+          { "role": "system", "content": "You are a helpful assistant." },
+          { "role": "user", "content": input.prompt },
+        ]
+      })
+
+      return {
+        chatCompletion: chatCompletion.data,
+      }
+    }),
+
+  getModels: publicProcedure
+    .mutation(async () => {
+      const models = await openai.listModels();
+
+      return {
+        models: models.data,
+      }
+    }),
+
+  getTextCompletion: publicProcedure
+    .input(z.object({ prompt: z.string() }))
+    .mutation(async ({ input }) => {
+      const textCompletion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: input.prompt,
+        max_tokens: 2048
+      })
+
+      return {
+        textCompletion: textCompletion.data,
+      }
+    }),
+
+  getTranslation: publicProcedure
+    .input(z.object({
+      text: z.string(),
+      language: z.string(),
+    }))
+    .mutation(async ({ input }) => {
+      const translation = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Translate from english to ${input.language}:\n${input.text}`,
+        max_tokens: 2048
+      })
+
+      return {
+        translation: translation.data,
+      }
+    })
+
+
+
+})
+
